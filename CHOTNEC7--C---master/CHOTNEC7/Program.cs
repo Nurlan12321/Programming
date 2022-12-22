c79a1dbb4fc9dfb61ccf2828d66d40a320b995a0
Nurlan12321 <105660650+Nurlan12321@users.noreply.github.com>
2022-12-22 20:13:35 +0300

    Add files via upload

---
+++ CHOTNEC7--C---master/CHOTNEC7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CHOTNEC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] k = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 22 };
+            int[] n = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 22 };
+            for (int i = 0; i < 14; i++)
+            {
+                if (k[i] % 2 == 1)
+                    Console.WriteLine($"Нечтёные числа " + k[i]);
+            }
+            for (int i = 0; i < 14; i++)
+            {
+                if (n[i] % 2 == 0)
+                    Console.WriteLine($"Чтёные числа " + n[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
