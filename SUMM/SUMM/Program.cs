e69a2f393736a9b582b694465f4a648a8f14c4b0
Nurlan12321 <105660650+Nurlan12321@users.noreply.github.com>
2022-12-22 20:16:46 +0300

    Add files via upload

---
+++ SUMM/SUMM/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SUMM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((101 + 0) / 3);
+            Console.WriteLine(3.0e-6 * 10000000.1);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && true);
+            Console.WriteLine((false && false)&&(true && true));
+            Console.WriteLine((false&& false) && (true && true));
+            Console.ReadKey();
+        }
+    }
+}
