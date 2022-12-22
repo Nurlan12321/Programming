bda2c33b951b76c91a4dbf3872ceb51f60d32d6b
Nurlan12321 <105660650+Nurlan12321@users.noreply.github.com>
2022-12-22 20:14:11 +0300

    Add files via upload

---
+++ Far8--C---master/Far8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Far8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double A,B;
+            B = 0;
+            try
+            {
+                Console.WriteLine("Введите число");
+                A = Convert.ToDouble(Console.ReadLine());
+                B = (A - 32) * 5 / 9;
+            }
+            catch
+            {
+                Console.WriteLine("Введите число");
+                Console.ReadKey();
+            }
+            Console.WriteLine($"{B}°C");
+            Console.ReadKey();
+        }
+    }
+}
