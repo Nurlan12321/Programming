ac0b203319927db478fbdf2065c31d8eaac4bc7a
Nurlan12321 <105660650+Nurlan12321@users.noreply.github.com>
2022-12-22 20:16:20 +0300

    Add files via upload

---
+++ mass-5-C--master/mass 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mass_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] k = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 22 };
+            int sum =0;
+        for (int i =0; i<k.Length; i++)
+                sum += k[i];
+        int sred = sum / k.Length;
+            for (int ar = 0; ar < 14; ar++)
+            {
+                if (k[ar] > sred)
+                {
+                    Console.WriteLine(k[ar]);
+
+                }
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
