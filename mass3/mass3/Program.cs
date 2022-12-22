bb9b182b146344f9cab8d5fcf27d54bc323b49c0
Nurlan12321 <105660650+Nurlan12321@users.noreply.github.com>
2022-12-22 20:15:39 +0300

    Add files via upload

---
+++ mass3/mass3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mass3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] k = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 22 };
+            int mass;
+            mass = k.Max();
+            Console.WriteLine(mass);
+            Console.ReadLine();
+        }
+    }
+}
