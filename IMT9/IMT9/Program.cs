541855930423c9692446ad494727ce1f3a23a170
Nurlan12321 <105660650+Nurlan12321@users.noreply.github.com>
2022-12-22 20:14:51 +0300

    Add files via upload

---
+++ IMT9/IMT9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IMT9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Ros, Ves, IMT;
+            Console.WriteLine("Введите рост");
+            Ros = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите вес");
+            Ves = Convert.ToDouble(Console.ReadLine());
+            IMT = Ves / Ros;
+            Console.WriteLine(IMT);
+            Console.ReadKey();
+        }
+    }
+}
