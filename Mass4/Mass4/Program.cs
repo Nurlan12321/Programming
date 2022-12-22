2227d32d88990c11a60fdb0e265e2bb83f2161cc
Nurlan12321 <105660650+Nurlan12321@users.noreply.github.com>
2022-12-22 20:16:04 +0300

    Add files via upload

---
+++ Mass4/Mass4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Mass4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] k = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 22 };
+            int mass;
+            mass = k.Min();
+            Console.WriteLine(mass);
+            Console.ReadLine();
+        }
+    }
+}
